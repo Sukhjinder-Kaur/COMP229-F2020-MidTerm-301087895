5b95012ddd7188c8e61ab5056764200f188086ae
Sukhjinder-Kaur <sukhmanyu@gmail.com>
2020-10-28 20:37:22 -0400

    Formatting and DB connection update

--- server/config/app.js
+++ server/config/app.js
@@ -1,3 +1,10 @@
+/*
+File name: index.ejs
+Author: Sukhjinder Kaur
+StudentID: 301087895
+WebApp: COMP229-F2020-MidTerm-301087895
+*/
+
 // moddules for node and express
 let createError = require('http-errors');
 let express = require('express');
@@ -10,11 +17,11 @@ let mongoose = require('mongoose');
 // URI
 let DB = require('./db');
 
-mongoose.connect(process.env.URI || DB.URI, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.URI || DB.URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let mongoDB = mongoose.connection;
 mongoDB.on('error', console.error.bind(console, 'Connection Error:'));
-mongoDB.once('open', ()=> {
+mongoDB.once('open', () => {
   console.log("Connected to MongoDB...");
 });
 
@@ -43,12 +50,12 @@ app.use('/books', books);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,10 @@
+/*
+File name: index.ejs
+Author: Sukhjinder Kaur
+StudentID: 301087895
+WebApp: COMP229-F2020-MidTerm-301087895
+*/
+
 module.exports = {
-  //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://sukhmanyu:49IVhnNu56QD83X6@mongodbserver.1ba5p.mongodb.net/books229?retryWrites=true&w=majority"
 };
